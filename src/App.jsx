851d21f616e5f6e97e752b843d4aac6952c33608
Loamy28 <83328154+Loamy28@users.noreply.github.com>
2023-06-10 13:58:03 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-slate-500 text-white text-xl">Hello World</h1>
+      <h1 className="bg-slate-500 text-white text-xl">KODEGO PALENGKE</h1>
     </>
   )
 }
